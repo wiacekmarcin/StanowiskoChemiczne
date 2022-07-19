d45bfd560388448a965451f59773c3d44e298ff4
Marcin Wiącek <marcin.wiacek@transbit.com.pl>
2022-07-19 08:38:16 +0200

    Nowa wersja

--- app/version.h
+++ app/version.h
@@ -2,7 +2,7 @@
 #define VERSION_H
 
 #define VERSION_MAJOR 1
-#define VERSION_MINOR 1
+#define VERSION_MINOR 2
 
 #define VERSION_DATE __DATE__
 //1.1 - Dodano możliwość sterowania dozownikime w konfiguracji
